ef2631fb3583486dac07433d2f946668deae334b
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-04 09:00:03 +0900

    Update app.py

--- orders/app.py
+++ orders/app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
-# FastAPI クラスのインスタンスを作成。このオブジェクトは API アプリケーションを表す 
 app = FastAPI(debug=True)
 
-# api モジュールをインポートし, 読み込み時にビュー関数を登録できるようにする 
+
 from orders.api import api
